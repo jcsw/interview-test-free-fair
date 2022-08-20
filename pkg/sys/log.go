09a926709d8a06aaab11377ce197c43c44bfaaf0
Jonas Almeida Cezar <jcezar.al@gmail.com>
2022-08-20 19:55:22 -0300

    Adicionando envio de logs para arquivo

--- pkg/sys/log.go
+++ pkg/sys/log.go
@@ -1,14 +1,24 @@
 package sys
 
 import (
+	"fmt"
 	log "log"
 	os "os"
+	"time"
 )
 
 var logger = initialize()
 
 func initialize() *log.Logger {
-	return log.New(os.Stdout, "[app] ", log.LstdFlags)
+
+	logDate := time.Now().Format("2006-01-02")
+	logFileName := fmt.Sprintf("./logs/%v.log", logDate)
+	file, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	return log.New(file, "[app] ", log.LstdFlags)
 }
 
 // LogDebug - Logging in level DEBUG
